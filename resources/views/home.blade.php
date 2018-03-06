9b2b4b391c098794c29f9b4c29e964cb052bbb3d
Luco <lucianoperezt@gmail.com>
2018-03-06 19:05:12 -0300

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .placeholder {
+                width: 100px;
+                height: 100px;
+                display: block;
+                background: black;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div>
+            <input type="text" id="btn-buscar" />
+            {{--<h1>Postura sobre la cabeza</h1>--}}
+            <h1>Titulo</h1>
+                {{--<img src="http://www.ayurvedayogashram.com/images/head/sirsasana.jpg" width="" height=""/>--}}
+                <div class="placeholder"></div>
+            <ul>
+                <li>
+                    Ayuda  en  las  actividades  fisiológicas  del cerebro mejorando la salud en general.
+                </li>
+                <li>
+                    En  esta  posición  se  fortalece  la  columna vertebral. 
+                </li>
+                <li>
+                    El peso de la columna vertebral se invierte relajando la parte lumbar. Esto ayuda a aliviar los  dolores de la parte baja de la  espalda (típicos por el estrés).
+                </li>
+                <li>
+                    Esta posición le da una pausa al corazón puesto que no tiene que bombear la sangre desde las piernas hasta el corazón gracias a  la  ley de gravedad. 
+                </li>
+                <li>
+                    Al estar en una posición invertida fluye naturalmente más sangre al corazón fortaleciéndolo y haciendo que bombee más sangre mejorando la  circulación  en  general  y  por  lo  tanto,  nutriendo,  oxigenando  y desintoxicando todos los tejidos del cuerpo. 
+                </li>
+                <li>
+                    Mejora problemas renales y estreñimiento.
+                </li>
+                <li>
+                    Cuando estamos en esta postura exhalamos más profundamente lo que ayuda a limpiar los pulmones y a aliviar el asma
+                </li>
+            </ul>
+        </div>
+    </body>
+</html>
