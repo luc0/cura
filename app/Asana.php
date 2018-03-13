6426b1a377d163fa027750dc47cf82781297d175
Luco <lucianoperezt@gmail.com>
2018-03-13 19:22:22 -0300

    - Inicializando la base de datos con los modelos, bien basico.

---
+++ app/Asana.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Asana extends Model
+{
+    protected $table = 'asanas';
+
+}
--- database/migrations/2018_03_13_215500_create_benefits_table.php
+++ database/migrations/2018_03_13_215500_create_benefits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Asanas extends Migration
+class CreateBenefitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class Asanas extends Migration
      */
     public function up()
     {
-        Schema::create('asanas', function (Blueprint $table) {
+        Schema::create('benefits', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('description');
+            $table->integer('body_id')->unsigned()->foreign('body_id')->references('id')->on('body');
+            $table->integer('disease_id')->unsigned()->foreign('disease_id')->references('id')->on('diseases');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class Asanas extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('asanas');
+        Schema::dropIfExists('benefits');
     }
 }
